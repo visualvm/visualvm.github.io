b932b5213ea6788ec752212556e1bec66d21726e
Jiri Sedlacek <jiri.sedlacek@oracle.com>
2020-03-30 16:56:31 +0200

    Banner to inform about possible upgrade of Java VisualVM.

--- js/versioncheck.js
+++ js/versioncheck.js
@@ -3,12 +3,24 @@ function checkVersion(pagebase) {
         var version = window.location.search.substring(1);
 
         if (version.startsWith("VisualVM_1")) document.getElementById("header_div").innerHTML += visualVMUpgrade(pagebase, version);
+        else if (version.startsWith("Java_VisualVM")) document.getElementById("header_div").innerHTML += javaVisualVMUpgrade(pagebase);
     }
 }
 
 function visualVMUpgrade(pagebase, oldversion) {
     var target = pagebase + "/download.html";
     var message = "Still using " + oldversion.replace("_", " ") + "? <a style='margin-left: 5px;' href='" + target + "'>Download the latest version!</a>";
+
+    return "\
+        <div class='header_alert'>\
+            <center>" + message + "</center>\
+        </div>\
+    ";
+}
+
+function javaVisualVMUpgrade(pagebase) {
+    var target = pagebase + "/javavisualvm.html";
+    var message = "Using Java VisualVM? <a style='margin-left: 5px;' href='" + target + "'>Learn how to upgrade to the latest VisualVM version!</a>";
 
     return "\
         <div class='header_alert'>\
